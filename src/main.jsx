237167af5933ab3680e616f3e29b5e6a58990eaa
Muhammad Abdullah Shakoor <abdullahshakoor1000@outlook.com>
2024-11-04 18:54:24 +0500

    merged

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomePage from './HomePage.jsx'
+import HomePage from './pages/HomePage.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 import AuthForm from './pages/AuthForm.jsx';
